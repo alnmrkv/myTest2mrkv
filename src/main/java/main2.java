5e2f4b954e900a02e8e671bfaf4131785e9125df
alnmrkv <aln.mrkv@mail.ru>
2021-10-03 23:44:42 +0300

    Initial commit

---
+++ src/main/java/main2.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class main2 {
+    public static void main (String[] args){
+        System.setProperty("webdriver.chrome.driver", "C:\\chromedriver\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+        WebElement input = driver.findElement(By.xpath("//Input[@aria-label='Найти']") );
+        input.click();
+        input.sendKeys("Калькулятор\n");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        input = driver.findElement(By.xpath("//div[@jsname='abcgof']"));input.click();
+        input = driver.findElement(By.xpath("//div[@jsname='WxTTNd']"));input.click();
+        input = driver.findElement(By.xpath("//div[@jsname='bkEvMb']"));input.click();
+        input = driver.findElement(By.xpath("//div[@jsname='Pt8tGc']"));input.click(); //=
+        driver.close();
+
+
+
+    }
+}
+
